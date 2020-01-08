815d8bad90c6aa5e7ffc1803fe9edec6fd6b6031
Sam Markham <sam.e.markham@gmail.com>
2020-01-07 17:26:02 -0800

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,24 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString) {
+  let timeArray = timeString.split(":")
+  let timeNumber = timeArray.join('')
+  let timeNumberInt = parseInt(timeNumber)
+  // console.log(timeNumberInt)
+  if (timeNumberInt <= 1200) {
+    return "Good Morning"
+  } else if (timeNumberInt > 1200 && timeNumberInt < 1700) {
+    return "Good Afternoon"
+  } else if (timeNumberInt >= 1700) {
+    return "Good Evening"
+  } else {
+    return "Error"
+  }
+};
+
 /* Write your implementation of displayMessage() */
+function displayMessage(newMessage) {
+  let heading = document.querySelector("h1#greeting")
+  heading.innerText = newMessage
+};
