88171b857b58591688abcd92101e2c116c39ebb3
ZumbaIdol <david.a.foote@gmail.com>
2020-04-22 22:28:34 -0500

    Done.

--- index.js
+++ index.js
@@ -10,25 +10,17 @@ function handleClick(e) {
 /* Write your implementation of greet() */
 
 function greet(string) {
-  let message = ""
   let splitString = string.split(':')
   let time = splitString[0]
   let newTime = parseInt(time, 10)
 
-  if (newTime < 12) {
-    message = "Good Morning"
-  } else if (newTime > 17) {
-    message = "Good Evening"
-  } else {
-    message = "Good Afternoon"
-  }
-
-  return message
+  if (newTime < 12) return "Good Morning"
+  if (newTime > 17) return "Good Evening"
+  return "Good Afternoon"
 }
 
 /* Write your implementation of displayMessage() */
 
-function displayMessage(string) {
-  const greeting = document.querySelector('h1')
-  greeting.innerText = string
+function displayMessage(greeting) {
+  document.getElementById("greeting").innerText = greeting
 }
